34f0a2e5089742bb44b0e011cd2614be806275b6
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-09-20 15:30:46 -0600

    commit tree d6374543eebaa787d79c0bbef9a30ae3ffe54aae

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+function writeCards (array) {
+    let thankYous = []
+    for(let i = 0; i < array.length; i++) 
+    {
+        thankYous.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+    }
+    return thankYous;
+}
+
+
+function countDown(num) {
+    for (let i = num; i >= 0; i-- ) {
+        console.log(num)
+        num = num - 1
+    }
+}
+
+countDown(10)
